ba9b3030eaac3bcfff1b153f8222e3df7a6a6a8c
oleksandr matviienko <omatvi@softserveinc.com>
2022-01-19 13:28:39 +0200

    User and article entities were added

---
+++ src/main/java/com/example/task2/entity/UserEntity.java
@@ -0,0 +1,65 @@
+package com.example.task2.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.UUID;
+
+@Entity
+@Table (name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "id", updatable = false, nullable = false, unique = true)
+    private UUID id;
+
+    @Column (name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column (name = "name")
+    private String name;
+
+    @Column (name = "password")
+    private String password;
+
+    public UserEntity() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
